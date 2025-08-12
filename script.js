9e405cf184b9617396402b6316ba360c00cc7f25
udaycodess <cyberpoint805@gmail.com>
2025-08-12 17:50:50 +0530

    Number Guessing game by Javascript using if-else statement

---
+++ script.js
@@ -0,0 +1,13 @@
+let gameNum = 25;
+let guessNum = prompt("Guess the game no.");
+if (gameNum == guessNum) {
+  console.log("you win the game");
+
+} else {
+  console.log("Incorrect guess ! try again!!");
+}
+
+
+
+
+
